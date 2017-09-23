b68692ac164f88d28583b248d2068cf7f546f80f
wk-j <somnuk.wk@gmail.com>
2017-09-24 00:02:56 +0700

    Installed rattle

--- Part3.R
+++ Part3.R
@@ -1,15 +1,19 @@
 library(rpart)
 
-#install.packages("RGtk2")
-#install.packages("carioDevice")
-# install.packages("rattle", repos="http://rattle.togaware.com", type="source")
-install.packages("RGtk2", dependencies = T, type = 'mac.binary')
+Sys.setenv(PKG_CONFIG_PATH = "/opt/local/lib/pkgconfig")
+Sys.setenv(PKG_CONFIG_PATH = "/usr/local/Cellar/gtk+/2.24.31_1/lib/pkgconfig")
+
+install.packages("RGtk2", repos="http://www.ggobi.org/r")
+install.packages("rattle", type="source")
+
 
 install.packages("rattle")
 install.packages("rpart.plot")
 install.packages("RColorBrewer")
 
 library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
 
 rm(list=ls())
 
@@ -19,9 +23,10 @@ test <- read.csv("input/test.csv", header = T)
 
 fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data = train, method = "class")
 
+# visualize
 plot(fit)
 text(fit)
-
+fancyRpartPlot(fit)
 
 prediction = predict(fit, test, type = "class")
 submmit <- data.frame(PassengerId = test$PassengerId, Survived = prediction)
